2aba2acd485dde654b0c884128eefc89e49df09e
Fergus Miles Ampuan <fergus.ampuan@gmail.com>
2024-08-01 14:53:05 +0800

    added dotenv for added security

--- index.js
+++ index.js
@@ -4,16 +4,18 @@ import bcrypt from "bcrypt";
 import session from "express-session";
 import passport from "passport";
 import { Strategy } from "passport-local";
+import env from "dotenv";
 
 const app = express();
 const port = 3000;
 const saltRounds = 10;
+env.config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.use(session({
-  secret: "SECRET",
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized:true,
   cookie:{
@@ -25,11 +27,11 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 const db = new pg.Client({
-  user: "postgres",
-  host: "localhost",
-  database: "secrets",
-  password: "root",
-  port: 5432,
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
 });
 db.connect();
 
